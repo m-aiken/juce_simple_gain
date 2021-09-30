327d816c187dc3296e5055586303f33a2a64e7d2
m-aiken <mattaiken91@gmail.com>
2021-09-30 18:34:34 +0100

    added basic visuals for gain slider

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -13,9 +13,15 @@
 SimpleGainAudioProcessorEditor::SimpleGainAudioProcessorEditor (SimpleGainAudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p)
 {
+    mGainSlider.setSliderStyle(juce::Slider::SliderStyle::LinearVertical);
+    mGainSlider.setTextBoxStyle(juce::Slider::TextEntryBoxPosition::TextBoxBelow, true, 50, 20);
+    mGainSlider.setRange(-60.f, 0.f, 0.01f);
+    mGainSlider.setValue(-6.f);
+    addAndMakeVisible(mGainSlider);
+
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    setSize (200, 300);
 }
 
 SimpleGainAudioProcessorEditor::~SimpleGainAudioProcessorEditor()
@@ -26,15 +32,13 @@ SimpleGainAudioProcessorEditor::~SimpleGainAudioProcessorEditor()
 void SimpleGainAudioProcessorEditor::paint (juce::Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.fillAll (juce::Colours::black);
 }
 
 void SimpleGainAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+
+    mGainSlider.setBounds(getWidth() / 2 - 50, getHeight() / 2 - 75, 100, 150);
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -25,6 +25,7 @@ public:
     void resized() override;
 
 private:
+    juce::Slider mGainSlider;
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     SimpleGainAudioProcessor& audioProcessor;
